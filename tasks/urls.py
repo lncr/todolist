70b6ff4115343990c73bc4fe044b19d182aa9916
Bektur <b.soltobaev@disoft.dev>
2021-08-12 20:12:25 +0600

    Set up CRUD for task

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from tasks.views import TaskAPIView
+from tasks.views import TaskAPIView, TaskDetailAPIView
 
 
 urlpatterns = [
     path('tasks/', TaskAPIView.as_view(), name='task_list_url'),
+    path('tasks/<int:id>/', TaskDetailAPIView.as_view(), name='task_detail_url')
 ]
