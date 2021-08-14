cd4a78b7146c750d4db598671071c8ef390ad6ba
Bektur <b.soltobaev@disoft.dev>
2021-08-14 20:58:12 +0600

    Change to model serializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
+from tasks.models import Task
 
 User = get_user_model()
 
@@ -12,10 +13,14 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', ]
 
 
-class TaskSerializer(serializers.Serializer):
+class TaskSerializer(serializers.ModelSerializer):
     creator = UserSerializer(read_only=True)
-    body = serializers.CharField()
-    estimated_finish_time = serializers.DateTimeField(format="%d-%m-%Y %H:%M",
-                                                      input_formats=['%d-%m-%Y %H:%M', ])
-    created_datetime = serializers.DateTimeField(read_only=True, format="%d-%m-%Y %H:%M")
-    is_completed = serializers.BooleanField(read_only=True)
+    estimated_finish_time = serializers.DateTimeField(format='%d-%m-%Y %H:%M',
+                                                     input_formats=['%d-%m-%Y %H:%M', ])
+    created_datetime = serializers.DateTimeField(format='%d-%m-%Y %H:%M')
+
+    class Meta:
+        model = Task
+        fields = ['id', 'creator', 'body', 'estimated_finish_time', 'created_datetime', 
+                  'is_completed']
+        read_only_fields = ['created_datetime', 'is_completed', ]
