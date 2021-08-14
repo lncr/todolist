47ed5b4f27766cd276ef43732ccca76b40f90e2d
Bektur <b.soltobaev@disoft.dev>
2021-08-14 19:49:31 +0600

    Update requirements

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,5 @@ from tasks.views import TaskAPIView, TaskDetailAPIView
 
 urlpatterns = [
     path('tasks/', TaskAPIView.as_view(), name='task_list_url'),
-    path('tasks/<int:id>/', TaskDetailAPIView.as_view(), name='task_detail_url')
+    path('tasks/<int:id>/', TaskDetailAPIView.as_view(), name='task_detail_url'),
 ]
